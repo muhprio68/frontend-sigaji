631ffd622c517b8a6cad334951e1facb20671716
muhprio68 <muhammadprio93@gmail.com>
2026-06-02 10:13:57 +0700

    ubah alamat backend kembali ke lokal

--- assets/js/config.js
+++ assets/js/config.js
@@ -2,8 +2,8 @@
 
 // 1. Variabel Global URL Backend
 // Kalau nanti di-hosting, tinggal ganti baris ini aja!
-//const API_BASE_URL = "http://localhost:8080/api";
-const API_BASE_URL = "https://backend-sigaji-production.up.railway.app/api"
+const API_BASE_URL = "http://localhost:8080/api";
+//const API_BASE_URL = "https://backend-sigaji-production.up.railway.app/api"
 // 2. Fungsi Global Format Rupiah (Biar gak nulis ulang terus)
 function formatRupiah(angka) {
     return "Rp " + (angka || 0).toLocaleString("id-ID");
