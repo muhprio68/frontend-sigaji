decd5de68d93322cb0fdbc1caa29f0da5b0bf201
muhprio68 <muhammadprio93@gmail.com>
2026-08-31 12:27:24 +0700

    update config

--- assets/js/config.js
+++ assets/js/config.js
@@ -2,7 +2,7 @@
 
 // 1. Variabel Global URL Backend
 // Kalau nanti di-hosting, tinggal ganti baris ini aja!
-const API_BASE_URL = "http://192.168.2.26:8080/api";
+const API_BASE_URL = "http://10.65.153.229:8080/api";
 //const API_BASE_URL = "https://backend-sigaji-production.up.railway.app/api"
 // 2. Fungsi Global Format Rupiah (Biar gak nulis ulang terus)
 function formatRupiah(angka) {
